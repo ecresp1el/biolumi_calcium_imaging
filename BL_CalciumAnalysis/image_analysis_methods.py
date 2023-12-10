6e42c6b46995d17e3dd0ee5b3547c22438a31d08
ecresp1el <cresp1el@cmich.edu>
2023-12-10 17:30:46 -0500

    updated method to detect files and error handling

--- BL_CalciumAnalysis/image_analysis_methods.py
+++ BL_CalciumAnalysis/image_analysis_methods.py
@@ -6,6 +6,11 @@ class ImageAnalysis:
 
     def list_files(self, folder_name):
         folder_path = os.path.join(self.project_folder, folder_name)
-        return [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+        try:
+            return [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+        except Exception as e:
+            print("Error accessing directory:", e)
+            return []
+
 
     # Additional methods can be added here
