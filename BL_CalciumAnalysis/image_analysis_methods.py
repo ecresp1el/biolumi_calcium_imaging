00af94635ac9d039e850165b456a032a0e4cb61b
ecresp1el <cresp1el@cmich.edu>
2023-12-10 17:38:45 -0500

    method updates to list directories and files

--- BL_CalciumAnalysis/image_analysis_methods.py
+++ BL_CalciumAnalysis/image_analysis_methods.py
@@ -3,14 +3,17 @@ import os
 class ImageAnalysis:
     def __init__(self, project_folder):
         self.project_folder = project_folder
-
+    
+    def list_directories(self):
+        return [d for d in os.listdir(self.project_folder) if os.path.isdir(os.path.join(self.project_folder, d))]
+    
     def list_files(self, folder_name):
         folder_path = os.path.join(self.project_folder, folder_name)
-        try:
-            return [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-        except Exception as e:
-            print("Error accessing directory:", e)
-            return []
+        all_files = []
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                all_files.append(os.path.join(root, file))
+        return all_files
 
 
     # Additional methods can be added here
